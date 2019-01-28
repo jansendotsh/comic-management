fce1396ee59edb55bceb445547be0282196baa19
Garrett Jansen <garrett@jansen.sh>
2019-01-27 19:42:03 -0500

    Refactoring

--- filesorting.py
+++ filesorting.py
@@ -4,39 +4,43 @@ import glob, os, subprocess, shutil, zipfile, untangle
 import datetime
 from sys import argv
 
-# Input files
-try:
-    workdir = argv[1]
-    os.chdir(workdir)
-except IndexError:
-    workdir = os.getcwd()
+def main():
+    # Grab working directory
+    try:
+        workdir = argv[1]
+        os.chdir(workdir)
+    except IndexError:
+        workdir = os.getcwd()
 
-# Directory to set final sorted files into, replace with argument pass
-finalDirectory = "/mnt/c/Users/janseng/Documents/comicScript"
+    # Final directory for files to move
+    finalDirectory = "/mnt/c/Users/janseng/Documents/comicScript"
 
-for file in glob.glob("*.cbz"):
-    zipped = zipfile.ZipFile(file)
+    for file in glob.glob("*.cbz"):
+        zipped = zipfile.ZipFile(file)
 
-    # Extract ComicInfo.xml
-    for i in zipped.namelist():
-        if i.endswith("ComicInfo.xml"):
-            zipped.extract(i, workdir)
-    
-    # Load XML file into Python
-    # Include RaiseError here in case there is no ComicInfo.xml -- Potentially write file names to stdout?
-    comicInfo = untangle.parse("ComicInfo.xml").ComicInfo
+        # Extract ComicInfo.xml
+        for i in zipped.namelist():
+            if i.endswith("ComicInfo.xml"):
+                zipped.extract(i, workdir)
+        
+        # Load XML file into Python
+        # Include RaiseError here in case there is no ComicInfo.xml -- Potentially write file names to stdout?
+        comicInfo = untangle.parse("ComicInfo.xml").ComicInfo
 
-    # Set essential values
-    comicVolume = comicInfo.Volume.cdata
-    comicSeries = comicInfo.Series.cdata
-    comicNumber = comicInfo.Number.cdata
-    comicPublisher = comicInfo.Publisher.cdata
-    comicDate = datetime.date(int(comicInfo.Year.cdata), int(comicInfo.Month.cdata), 1)
+        # Set essential values
+        comicVolume = comicInfo.Volume.cdata
+        comicSeries = comicInfo.Series.cdata
+        comicNumber = comicInfo.Number.cdata
+        comicPublisher = comicInfo.Publisher.cdata
+        comicDate = datetime.date(int(comicInfo.Year.cdata), int(comicInfo.Month.cdata), 1)
 
-    print("The comic in question is nummber %s of %s and was published by %s in %s, %s." % (comicNumber, comicSeries, comicPublisher, comicDate.strftime('%B'), comicDate.strftime('%Y')))
-    # Create folder
+        print("The comic in question is nummber %s of %s and was published by %s in %s, %s." % (comicNumber, comicSeries, comicPublisher, comicDate.strftime('%B'), comicDate.strftime('%Y')))
+        # Create folder
 
-    # Move file to newly formed folder
+        # Move file to newly formed folder
 
-    # Cleaning up ComicInfo.xml work is done
-    os.remove("ComicInfo.xml")
+        # Cleaning up ComicInfo.xml work is done
+        os.remove("ComicInfo.xml")
+
+if __name__ == '__main__':
+   main() 
