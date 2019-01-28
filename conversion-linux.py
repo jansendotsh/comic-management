50aa4766db88219daa4601ae06caf2505c6fa750
garrettjj <garrett@jansen.sh>
2019-01-28 02:38:42 -0500

    Linux-ifying the conversion script

--- conversion-linux.py
+++ conversion-linux.py
@@ -10,12 +10,13 @@ except IndexError:
     workdir = os.getcwd()
 
 for file in glob.glob("*.cbr"):
+    #break this into a separate function for easily running against *.CBR
     print(file) # Just for showing the files that are in use
     print(os.path.splitext(file)[0]) # Testing for dir creation
     os.mkdir(os.path.splitext(file)[0])
     # insert for loop to identify whether os.path.isfile == True for 7z.exe, that can make this compatible with Linux & Windows
-    subprocess.call(['7z', 'e', file, '-o'+os.path.splitext(file)[0]])
-    subprocess.call(['7z', 'a', '-tzip', os.path.splitext(file)[0]+".cbz", os.path.splitext(file)[0]])
+    subprocess.call(['unrar', 'x', file, os.path.splitext(file)[0]])
+    subprocess.call(['zip', '-r', os.path.splitext(file)[0]+".cbz", os.path.splitext(file)[0]])
     shutil.rmtree(os.path.splitext(file)[0])
     os.remove(file)
     print(file+"has been successfully converted.")
