f35ae9befc60ba6beed8e74ed3b9b70a854e8687
Garrett Jansen <garrett@jansen.sh>
2019-01-27 19:33:08 -0500

    Updating filesorting

--- filesorting.py
+++ filesorting.py
@@ -1,21 +1,42 @@
 #!/usr/bin/python3
 
-import glob, os, subprocess, shutil, zipfile
-from xml.dom import minidom
+import glob, os, subprocess, shutil, zipfile, untangle
+import datetime
 from sys import argv
 
+# Input files
 try:
     workdir = argv[1]
     os.chdir(workdir)
 except IndexError:
     workdir = os.getcwd()
 
-for file in glob.glob("*.cbz"):
-    metadata = zipfile.ZipFile(file, 'r')
-    metaRead = metadata.read('ComicInfo.xml')
-    mydoc = minidom.parseString(metaRead)
+# Directory to set final sorted files into, replace with argument pass
+finalDirectory = "/mnt/c/Users/janseng/Documents/comicScript"
 
-    items = mydoc.getElementsByTagName('item')
-    #print(items[1].attributes['Volume'].value)
-    for elem in items:  
-        print(elem.firstChild.data)
+for file in glob.glob("*.cbz"):
+    zipped = zipfile.ZipFile(file)
+
+    # Extract ComicInfo.xml
+    for i in zipped.namelist():
+        if i.endswith("ComicInfo.xml"):
+            zipped.extract(i, workdir)
+    
+    # Load XML file into Python
+    # Include RaiseError here in case there is no ComicInfo.xml -- Potentially write file names to stdout?
+    comicInfo = untangle.parse("ComicInfo.xml").ComicInfo
+
+    # Set essential values
+    comicVolume = comicInfo.Volume.cdata
+    comicSeries = comicInfo.Series.cdata
+    comicNumber = comicInfo.Number.cdata
+    comicPublisher = comicInfo.Publisher.cdata
+    comicDate = datetime.date(int(comicInfo.Year.cdata), int(comicInfo.Month.cdata), 1)
+
+    print("The comic in question is nummber %s of %s and was published by %s in %s, %s." % (comicNumber, comicSeries, comicPublisher, comicDate.strftime('%B'), comicDate.strftime('%Y')))
+    # Create folder
+
+    # Move file to newly formed folder
+
+    # Cleaning up ComicInfo.xml work is done
+    os.remove("ComicInfo.xml")
